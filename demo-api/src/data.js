c350a2de686fab101555259caf098e3bbee92d1b
Clarence Coppinger <clarence.coppinger@daugherty.com>
2019-05-15 09:45:27 -0500

    project setup

---
+++ demo-api/src/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { firstName: "Clarence", lastName: "Coppinger" },
+  { firstName: "Austin", lastName: "Smith" },
+  { firstName: "Conor", lastName: "Dickens" }
+];
+
+module.exports = data;
