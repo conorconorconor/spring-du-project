1832e7446aba51476a90f26e8dd38cfd09f0ef9c
Clarence Coppinger <clarence.coppinger@daugherty.com>
2019-05-15 16:21:44 -0500

    changes

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const User = require("../models/user");
 
-createUser = data => {
-  let user = new User();
-  user.firstName = data.firstName;
-  user.lastName = data.lastName;
-  user.save((err, document) => {
-    if (err) {
-      return err;
-    } else {
-      return document;
-    }
-  });
-};
-
-deleteUser = id => {
-  User.deleteOne(
-    {
-      _id: id
-    },
-    err => {
+export class UserService {
+  createUser(data) {
+    let user = new User();
+    user.firstName = data.firstName;
+    user.lastName = data.lastName;
+    user.save((err, document) => {
       if (err) {
         return err;
       } else {
-        const success = `You have successfully deleted user: ${req.params.id}`;
-        return success;
+        return document;
       }
-    }
-  );
-};
+    });
+  }
+
+  deleteUser = id => {
+    User.deleteOne(
+      {
+        _id: id
+      },
+      err => {
+        if (err) {
+          return err;
+        } else {
+          const success = `You have successfully deleted user: ${
+            req.params.id
+          }`;
+          return success;
+        }
+      }
+    );
+  };
+}
