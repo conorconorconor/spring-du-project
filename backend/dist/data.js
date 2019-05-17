b2dd474f098f19aa41b717a62a3743cbd9b742b5
Clarence Coppinger <clarence.coppinger@daugherty.com>
2019-05-17 08:05:23 -0500

    changed gitignore updated repo

--- backend/dist/data.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var data = [
-    { firstName: "Clarence", lastName: "Coppinger" },
-    { firstName: "Austin", lastName: "Smith" },
-    { firstName: "Conor", lastName: "Dickens" }
-];
-exports.default = data;
-//# sourceMappingURL=data.js.map
