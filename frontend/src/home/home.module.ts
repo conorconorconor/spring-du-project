264274d261141e227a668b27448ca6a3fac739cd
Clarence Coppinger <clarence.coppinger@daugherty.com>
2019-05-20 10:20:15 -0500

    added fake login

--- frontend/src/home/home.module.ts
+++ frontend/src/home/home.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
-import { HomeRoutingModule } from "./home-routing.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule, MatButtonModule, MatFormFieldModule } from "@angular/material";
 import { HomeComponent } from "./home.component";
+import { HomeRoutingModule } from './home-routing.module';
+
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    HomeRoutingModule
+  ],
   exports: [HomeComponent]
 })
-export class HomeModule {}
+export class HomeModule { }
