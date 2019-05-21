d4c732eee8b3b162a5c497ed9e337cc58464400a
ams0828 <austin.smith@daugherty.com>
2019-05-21 08:18:47 -0500

    created consulant-edit and changed css to scss in shared directory

---
+++ frontend/src/consultant/consultant-edit/consultant-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-consultant-edit",
+  templateUrl: "./consultant-edit.component.html",
+  styleUrls: ["../../shared/styles/consultant-edit.scss"]
+})
+export class ConsultantEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
