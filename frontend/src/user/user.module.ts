6abe05956467077c98ac90f57fff847a0e47e8cc
ams0828 <austin.smith@daugherty.com>
2019-05-23 14:46:20 -0500

    finished create users

--- frontend/src/user/user.module.ts
+++ frontend/src/user/user.module.ts
@@ -1,14 +1,50 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { UserRoutingModule } from './user-routing.module';
-import { UserComponent } from './user.component';
+import { UserRoutingModule } from "./user-routing.module";
+import { UserComponent } from "./user.component";
+import { UserCreateComponent } from "./user-create/user-create.component";
+import {
+  MatRadioModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatTableModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
+  MatListModule,
+  MatIconModule,
+  MatPaginatorModule
+} from "@angular/material";
+import { ConsultantRoutingModule } from "src/consultant/consultant-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ScrollDispatchModule } from "@angular/cdk/scrolling";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserCreateComponent],
   imports: [
     CommonModule,
+    MatRadioModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ConsultantRoutingModule,
+    MatTableModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatListModule,
+    ScrollDispatchModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatPaginatorModule,
     UserRoutingModule
   ]
 })
-export class UserModule { }
+export class UserModule {}
